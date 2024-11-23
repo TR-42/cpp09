1c85605d52a01c0bf9c7626247c4952f833389a5
kfujita <kfujita@student.42tokyo.jp>
2024-11-24 00:38:13 +0900

    tmp

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -5,6 +5,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <exception>
+#include <iostream>	 // DEBUG
+#include <iterator>
 
 PmergeMe::VALUE_TYPE PmergeMe::MAX = std::numeric_limits<PmergeMe::VALUE_TYPE>::max();
 PmergeMe::VALUE_TYPE PmergeMe::MIN = std::numeric_limits<PmergeMe::VALUE_TYPE>::min();
@@ -84,10 +86,101 @@ const PmergeMe::CONTAINER_TYPE_2 PmergeMe::getContainer2(
 	return this->_container2;
 }
 
+static void _recursive(
+	PmergeMe::CONTAINER_TYPE_1 &arr,
+	size_t depth,
+	size_t spanSize
+)
+{
+	if (arr.size() < spanSize)
+		return;
+
+	size_t spanSizeHalf = spanSize / 2;
+	for (
+		PmergeMe::CONTAINER_TYPE_1::iterator it = arr.begin();
+		std::distance(it, arr.end()) < spanSize;
+	) {
+		PmergeMe::CONTAINER_TYPE_1::iterator leftMaxIt = it + spanSizeHalf - 1;
+		PmergeMe::CONTAINER_TYPE_1::iterator rightMaxIt = leftMaxIt + spanSizeHalf;
+		PmergeMe::CONTAINER_TYPE_1::iterator nextIt = rightMaxIt + 1;
+		if (*rightMaxIt < *leftMaxIt) {
+			for (
+				PmergeMe::CONTAINER_TYPE_1::iterator leftIt = it, rightIt = leftMaxIt + 1;
+				rightIt != nextIt;
+				++rightIt, ++leftIt
+			) {
+				PmergeMe::VALUE_TYPE temp = *leftIt;
+				*leftIt = *rightIt;
+				*rightIt = temp;
+			}
+		}
+		it = nextIt;
+	}
+	_recursive(arr, depth + 1, spanSize * 2);
+	return;
+}
+
+typedef std::deque<std::deque<PmergeMe::VALUE_TYPE> > Sort1PairsType;
 void PmergeMe::sort1(
 )
 {
-	std::sort(this->_container1.begin(), this->_container1.end());
+	Sort1PairsType pairs;
+	for (
+		PmergeMe::CONTAINER_TYPE_1::iterator it = this->_container1.begin();
+		it != this->_container1.end();
+		++it
+	) {
+		PmergeMe::CONTAINER_TYPE_1::iterator nextIt = it + 1;
+		PmergeMe::CONTAINER_TYPE_1 pair;
+		if (nextIt == this->_container1.end()) {
+			pair.push_back(*it);
+		} else if (*it <= *nextIt) {
+			pair.push_back(*it);
+			pair.push_back(*nextIt);
+			++it;
+		} else {
+			pair.push_back(*nextIt);
+			pair.push_back(*it);
+			++it;
+		}
+		pairs.push_back(pair);
+	}
+
+	std::cout << "pairs.size(): " << pairs.size() << std::endl;
+
+	while (1 < pairs.size()) {
+		if (pairs.size() == 2 && pairs[0].size() != pairs[1].size()) {
+			break;
+		}
+
+		for (
+			Sort1PairsType::iterator leftIt = pairs.begin();
+			leftIt != pairs.end();
+			++leftIt
+		) {
+			Sort1PairsType::iterator rightIt = leftIt + 1;
+			if ((leftIt + 1) == pairs.end()) {
+				break;
+			}
+			PmergeMe::CONTAINER_TYPE_1::iterator insertToIt;
+			if (rightIt->back() < leftIt->back()) {
+				insertToIt = leftIt->begin();
+			} else {
+				insertToIt = leftIt->end();
+			}
+			std::cout << "leftIt->size(): " << leftIt->size() << std::endl;
+			std::cout << "rightIt->size(): " << rightIt->size() << std::endl;
+			leftIt->insert(insertToIt, rightIt->begin(), rightIt->end());
+
+			leftIt = pairs.erase(rightIt) - 1;
+		}
+	}
+
+	this->_container1 = pairs.front();
+	if (pairs.size() == 2) {
+		// DEBUG
+		this->_container1.insert(this->_container1.end(), pairs.back().begin(), pairs.back().end());
+	}
 }
 
 void PmergeMe::sort2(
